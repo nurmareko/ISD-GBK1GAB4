2d5bd3751036e2694356941792300626c4e40c17
nurmareko <nurmareko@yandex.ru>
2025-03-04 16:57:33 +0700

    Implement Node class

--- jurnal-03/Node.java
+++ jurnal-03/Node.java
@@ -1,3 +1,13 @@
-public class Node {
+public class Node<E> {
+    private E data;
+    private Node<E> next;
 
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public E getData() {
+        return data;
+    }
 }
