1a461bb6c010ea3241f0f817cebe89592d37fea9
nurmareko <nurmareko@yandex.ru>
2025-03-05 12:47:23 +0700

    Implement constructor for Recipe class

--- jurnal-03/Recipe.java
+++ jurnal-03/Recipe.java
@@ -1,9 +1,23 @@
+import org.w3c.dom.NameList;
+import org.w3c.dom.css.Rect;
+
 public class Recipe {
     private String name;
     private String mainComponent;
     private String cookingTime;
 
+    public Recipe(String name, String mainComponent, String cookingTime) {
+        this.name = name;
+        this.mainComponent = mainComponent;
+        this.cookingTime = cookingTime;
+    }
+
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "recipe: " + name + " " + mainComponent + " " + cookingTime + '\n';
     }
 }
