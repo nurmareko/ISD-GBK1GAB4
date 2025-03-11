2dc466fa68777a49d39b4afac021b98b005cc548
nurmareko <nurmareko@yandex.ru>
2025-03-11 10:49:08 +0700

    Initialize Main.java with stub methods

--- jurnal-04/Main.java
+++ jurnal-04/Main.java
@@ -1,5 +1,33 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    // Remove a book; return true if successful
+    public boolean removeBook(String isbn) {
+        return false;
+    }
+
+    // Edit book's title; return true if successful
+    public boolean setTitle(String isbn) {
+        return false;
+    }
+
+    // Edit book's loan status; return true if successful
+    public boolean setHasLoan(String isbn) {
+        return false;
+    }
+
+    // Display all book in a list
+    public void printAll() {
+
+    }
+
+    // Display all book in a list in reverse order (last entry to fist)
+    public void printAllReverse() {
 
     }
 }
