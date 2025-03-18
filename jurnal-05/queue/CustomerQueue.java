af918c2e5b25735353b8f77a9f82a62fa138924f
nurmareko <nurmareko@yandex.ru>
2025-03-18 13:30:12 +0700

    Add customer queue implementation with skeleton methods

--- jurnal-05/queue/CustomerQueue.java
+++ jurnal-05/queue/CustomerQueue.java
@@ -1,3 +1,27 @@
-public class CustomerQueue {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class CustomerQueue {
+    private Queue<String> queueList;
+    private int departed;
+
+    public CustomerQueue() {
+        queueList = new LinkedList<>();
+        departed = 0;
+    }
+
+    // Print all
+    public String printAll() {
+        return "";
+    }
+
+    // Get number of people who in the queue
+    public int getLength() {
+        return 0;
+    }
+
+    // Get number of people who left the queue
+    public int getDeparted() {
+        return 0;
+    }
 }
