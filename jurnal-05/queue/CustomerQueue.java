04323bdefb4efa15a2da760482d13e22fa9e2048
nurmareko <nurmareko@yandex.ru>
2025-03-18 14:24:04 +0700

    Implement queue operations with length tracking

--- jurnal-05/queue/CustomerQueue.java
+++ jurnal-05/queue/CustomerQueue.java
@@ -3,25 +3,43 @@ import java.util.Queue;
 
 public class CustomerQueue {
     private Queue<String> queueList;
+    private int length;
     private int departed;
 
     public CustomerQueue() {
         queueList = new LinkedList<>();
+        length = 0;
         departed = 0;
     }
 
+    // Enqueue
+    public void enqueue(String name) {
+        length++;
+        queueList.offer(name);
+    }
+
+    // Dequeue; return name
+    public String dequeue() {
+        length--;
+        departed++;
+        return queueList.poll();
+    }
+
     // Print all
-    public String printAll() {
-        return "";
+    public void printAll() {
+        for (String people : queueList) {
+            System.out.printf("%s ", people);
+        }
+        System.out.println();
     }
 
     // Get number of people who in the queue
     public int getLength() {
-        return 0;
+        return length;
     }
 
     // Get number of people who left the queue
     public int getDeparted() {
-        return 0;
+        return departed;
     }
 }
