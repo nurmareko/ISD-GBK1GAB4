19e1a9afd81008dfb8bcb3caaae1ab177935f9c3
nurmareko <nurmareko@yandex.ru>
2025-03-18 16:53:01 +0700

    Add bracket validation check using Stack

--- jurnal-05/stack/Main.java
+++ jurnal-05/stack/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(final String args[]) {
+import java.util.Stack;
 
+public class Main {
+    public static void main(String args[]) {
+        System.out.println(checkBracket("[({})]"));
+        System.out.println(checkBracket("[]{}()"));
+        System.out.println(checkBracket("[{)]"));
+        System.out.println(checkBracket("[{"));
+    }
+
+    public static boolean checkBracket(String str) {
+        char[] inputAsChar = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < inputAsChar.length; i++) {
+            char c = inputAsChar[i];
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']' || c == ')' || c == '}') {
+                char leftSide = stack.pop();
+                if (leftSide == '[' && c == ']') {
+                    return true;
+                } else if (leftSide == '(' && c == ')') {
+                    return true;
+                } else if (leftSide == '{' && c == '}') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
