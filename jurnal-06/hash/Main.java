a5dba41eacaac8d744f9d41275cf586416b15a8c
nurmareko <nurmareko@yandex.ru>
2025-03-25 14:00:44 +0700

    Add HashSet test case for hash table implementation

---
+++ jurnal-06/hash/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        HashSet table = new HashSet(3);
+
+        table.add("date");
+        table.add("aiba");
+        table.add("mizuki");
+        table.add("ryuki");
+        table.add("ota");
+        table.add("iris");
+        table.add("tama");
+
+        table.print();
+    }
+}
