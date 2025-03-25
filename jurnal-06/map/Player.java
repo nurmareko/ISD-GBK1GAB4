bc9ee44e0ec50a18a48cff293e01eb77b09be3be
nurmareko <nurmareko@yandex.ru>
2025-03-25 17:14:38 +0700

    Add player number field and implement hashCode method

--- jurnal-06/map/Player.java
+++ jurnal-06/map/Player.java
@@ -2,15 +2,27 @@ public class Player {
     private String name;
     private int age;
     private String position;
+    private String number;
 
-    public Player(String name, int age, String position) {
+    public Player(String name, int age, String position, String number) {
         this.name = name;
         this.age = age;
         this.position = position;
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        final int MULTIPLIER = 7;
+        int hash = 1;
+        for (int i = 0; i < number.length(); i++) {
+            hash = hash * MULTIPLIER + number.charAt(i);
+        }
+        return hash;
     }
 
     @Override
     public String toString() {
-        return "{" + name + ", " + age + ", " + position + "}";
+        return "{" + name + ", " + age + ", " + position + ", " + number + "}";
     }
 }
