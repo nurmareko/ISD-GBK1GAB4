f7e76c112f638e6890c0f7d6502f8985cb60ca61
nurmareko <nurmareko@yandex.ru>
2025-04-11 03:54:43 +0700

    Add Member class with basic attributes and constructor

---
+++ pra-assessment-01/C/Member.java
@@ -0,0 +1,15 @@
+public class Member {
+    private String phone;
+    private String name;
+    private String address;
+    private String type;
+    private int year;
+
+    public Member(String phone, String name, String address, String type, int year) {
+        this.phone = phone;
+        this.name = name;
+        this.address = address;
+        this.type = type;
+        this.year = year;
+    }
+}
