6afff0c88f7253eeda7d51b6b5f7ceddabfa0812
nurmareko <nurmareko@yandex.ru>
2025-04-11 04:10:28 +0700

    Add Members class skeleton with empty methods

---
+++ pra-assessment-01/C/Members.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+
+public class Members {
+    private LinkedList<Member> members;
+
+    public Members() {
+        members = new LinkedList<>();
+    }
+
+    public void add(Member newMemeber) {
+
+    }
+
+    public void remove(String name) {
+
+    }
+
+    public void find(String name) {
+
+    }
+
+    public void filter(int year) {
+
+    }
+}
