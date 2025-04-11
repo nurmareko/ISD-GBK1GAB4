c361ef852a6a402920a42ea5ce661262f546b7f5
nurmareko <nurmareko@yandex.ru>
2025-04-11 16:32:07 +0700

    Add WebStack class skeleton with navigation stacks

---
+++ pra-assessment-01/E/WebStack.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class WebStack {
+    Stack<String> back;
+    Stack<String> forward;
+
+    public WebStack() {
+        back = new Stack<>();
+        forward = new Stack<>();
+    }
+
+    public void add(String url) {
+
+    }
+
+    public void back() {
+
+    }
+
+    public void forward() {
+
+    }
+
+    public void history() {
+
+    }
+}
