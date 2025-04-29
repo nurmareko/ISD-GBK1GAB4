05ceac526bf006c08ff57428fe5dd396f3bb5553
nurmareko <nurmareko@yandex.ru>
2025-04-29 22:02:44 +0700

    Add skeleton Graph class with adjacency list implementation

---
+++ jurnal-09/Graph.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    private HashMap<Character, ArrayList<Character>> AdjacencyList;
+
+    public Graph() {
+        AdjacencyList = new HashMap<>();
+    }
+
+    public void addEdge(char from, char to) {
+
+    }
+
+    public void breadthFirstSearch() {
+
+    }
+
+    public void DepthFirstSearch() {
+
+    }
+}
