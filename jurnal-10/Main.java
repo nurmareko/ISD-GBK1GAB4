fc741aeff32f31e62d55e9ffa7b2a24ab7e50637
nurmareko <nurmareko@yandex.ru>
2025-05-06 16:24:58 +0700

    Remove unused commented lines

--- jurnal-10/Main.java
+++ jurnal-10/Main.java
@@ -5,15 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
 
-        // graph.addNode('A', new ArrayList<>(Arrays.asList('B', 'D', 'E')));
-        // graph.addNode('B', new ArrayList<>(Arrays.asList('C', 'E', 'G')));
-        // graph.addNode('C', new ArrayList<>(Arrays.asList('G')));
-        // graph.addNode('D', new ArrayList<>(Arrays.asList('E')));
-        // graph.addNode('E', new ArrayList<>(Arrays.asList('F')));
-        // graph.addNode('F', new ArrayList<>());
-        // graph.addNode('G', new ArrayList<>(Arrays.asList('F')));
-        // graph.addNode('Z', new ArrayList<>());
-
         graph.addNode("Hologram", new ArrayList<>(Arrays.asList("Kostum")));
         graph.addNode("Kostum",
                 new ArrayList<>(Arrays.asList("Pencahayaan", "Pengepasan kostum", "Pembuatan panggung")));
@@ -24,10 +15,6 @@ public class Main {
         graph.addNode("Persiapan panggung", new ArrayList<>(Arrays.asList("Pembuatan panggung")));
         graph.addNode("Properti panggung", new ArrayList<>(Arrays.asList("Kostum", "Persiapan panggung")));
 
-        // graph.breadthFirstSearch("Hologram");
-        // System.out.println();
-        // graph.depthFirstSearch("Hologram");
-        // System.out.println();
         graph.topologicalSort();
     }
 }
